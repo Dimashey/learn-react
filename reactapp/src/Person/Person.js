539fc68003842bbc5ae60a0f0aa8749debd7b215
Dmytro Sheiko <sheykodmitriyjob@gmail.com>
2019-11-06 00:16:05 +0200

    begin css module

--- reactapp/src/Person/Person.js
+++ reactapp/src/Person/Person.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 class Person extends Component {
 
     render() {
     
         return (
-            <div className="Person">
+            <div className={classes.Person}>
                 <p onClick={this.props.click}>I'm a {this.props.name} and i am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
