9fc5a6594b973c35f653dd10f5ae2cff771e3d34
OleksandrGorbatov <Oleksandr.Gorbatov@NearShorePartner.ch>
2019-12-05 19:29:42 +0200

    added side drawer

--- burger-project/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-project/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,8 +3,8 @@ import classes from './NavigationItem.css';
 
 const navigationItem = ( props ) => {
     return (
-        <li className={classes.NavigationItem}><
-            a href={props.link}
+        <li className={classes.NavigationItem}>
+            <a href={props.link}
               className={props.active ? classes.active : null}
             >{props.children}</a></li>
     );
