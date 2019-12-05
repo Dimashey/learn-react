99338924a2dda3f19ae1099e7ab0951899a3459b
Dmytro Sheiko <sheykodmitriyjob@gmail.com>
2019-12-05 23:26:31 +0200

    finished section 7

--- burger-project/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-project/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxilliary'
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxilliary'
 import Button from '../../UI/Button/Button';
 
-const olderSummary = ( props ) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
+class OrderSummary extends Component {
+    render() {
+        const ingredientSummary = Object.keys( this.props.ingredients )
+            .map(igKey => {
+                return (
                     <li style={{textTransform: 'capitalize'}} key={igKey}>
-                        <span>{igKey}</span>: {props.ingredients[igKey]}
+                        <span>{igKey}</span>: {this.props.ingredients[igKey]}
                     </li>);
-        });
-    return (
-        <Aux>;
-            <h3>Yo;ur Order</h3>
-            <p>A ;delicious burger with the following ingredients: </p>
-            <ul>;
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+            });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default olderSummary;
+export default OrderSummary;
--- burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxilliary'
+import Aux from '../../hoc/Auxiliary/Auxilliary'
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/Buildcontrols';
 import Modal from '../../components/UI/Modal/Modal';
