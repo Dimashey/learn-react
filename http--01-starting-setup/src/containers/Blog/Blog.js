58d3ef1bc0d861a67592754d28e4ec60a8c3f270
OleksandrGorbatov <Oleksandr.Gorbatov@NearShorePartner.ch>
2019-12-12 19:44:08 +0200

    nested routing section

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,62 +1,36 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
+// import axios from '../../axios/';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    };
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-            })
-            .catch(() => {
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState( {selectedPostId: id});
-    };
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-
-        if (!this.state.posts) {
-            posts = this.state.posts.map(post => {
-                return  <Post key={post.id}
-                              title={post.title}
-                              author={post.author}
-                              clicked={() => this.postSelectedHandler(post.id)}
-                />;
-            });
-        }
 
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink to="/posts/"
+                                         exact>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Switch>
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts"/>
+                    {/*<Route path="/" component={Posts} />*/}
+                </Switch>
             </div>
         );
     }
