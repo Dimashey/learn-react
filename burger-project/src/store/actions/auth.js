fff9f33d28cac49d0546bab2829915e3d47a3caa
Dmytro Sheiko <sheykodmitriyjob@gmail.com>
2020-01-05 11:52:26 +0200

    addind authentication

---
+++ burger-project/src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId
+    };
+};
+
+export const authFail = (error) => {
+    return {
+         type: actionTypes.AUTH_FAIL,
+         error: error
+    };
+};
+
+export const auth = (email, password, isSignUp) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email,
+            password,
+            returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDmBW4mGRLlcDXAIVbucrkVUA-RXWt5COw';
+        if (!isSignUp) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDmBW4mGRLlcDXAIVbucrkVUA-RXWt5COw';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
+    };
+};
