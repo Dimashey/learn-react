a92a555fd28c85863777ac9112db4e2e5dc74bbb
Dmytro Sheiko <sheykodmitriyjob@gmail.com>
2020-02-10 23:49:15 +0200

    learned features to replace redux

---
+++ replace-redux/src/containers/Products.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import ProductItem from '../components/Products/ProductItem';
+import { useStore } from '../hooks/store';
+import './Products.css';
+
+const Products = props => {
+  const [{initialState : state}] = useStore();
+  return (
+    <ul className="products-list">
+      {state.products.map(prod => (
+        <ProductItem
+          key={prod.id}
+          id={prod.id}
+          title={prod.title}
+          description={prod.description}
+          isFav={prod.isFavorite}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Products;
